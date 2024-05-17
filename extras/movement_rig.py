fccb10d6194ad96e4a57212c7e67466652d8b4e1
GONCALOUNI <a76967@ualg.pt>
2024-05-17 23:25:36 +0100

    nova versao

--- extras/movement_rig.py
+++ extras/movement_rig.py
@@ -29,7 +29,7 @@ class MovementRig(Object3D):
         self.KEY_MOVE_RIGHT = "d"
         self.KEY_JUMP = "space"  # New key for jumping
         self.KEY_MOVE_UP = "z"
-        #self.KEY_MOVE_DOWN = "left ctrl"
+        self.KEY_MOVE_DOWN = "left ctrl"
         self.KEY_TURN_LEFT = "q"
         self.KEY_TURN_RIGHT = "e"
         self.KEY_LOOK_UP = "t"
@@ -70,8 +70,8 @@ class MovementRig(Object3D):
                     self.KEY_MOVE_RIGHT = ""
                 elif key == "z":
                     self.KEY_MOVE_UP = ""
-                #elif key == "left ctrl":
-                 #   self.KEY_MOVE_DOWN = ""
+                elif key == "left ctrl":
+                    self.KEY_MOVE_DOWN = ""
 
         # Restaurar as teclas permitidas
         #self.allow_movement()
@@ -86,7 +86,7 @@ class MovementRig(Object3D):
         self.KEY_MOVE_LEFT = "a"
         self.KEY_MOVE_RIGHT = "d"
         self.KEY_MOVE_UP = "z"
-        #self.KEY_MOVE_DOWN = "left ctrl"
+        self.KEY_MOVE_DOWN = "left ctrl"
 
     def update(self, input_object, delta_time, collision=False):
         move_amount = self._units_per_second * delta_time
@@ -116,10 +116,6 @@ class MovementRig(Object3D):
                 self.is_jumping = False
                 self.jump_speed = 10  # Reset jump speed
 
-        #Restrict downward movement if the model is already on the ground
-        if self.global_position[1] <= 0:
-            move_amount = max(0, move_amount)  # Set move_amount to zero to restrict downward movement
-
         self.keys_pressed = [key for key in input_object.key_pressed_list]
 
         if collision:
@@ -137,8 +133,8 @@ class MovementRig(Object3D):
             self.translate(move_amount, 0, 0)
         if input_object.is_key_pressed(self.KEY_MOVE_UP):
             self.translate(0, move_amount, 0)
-        #if input_object.is_key_pressed(self.KEY_MOVE_DOWN):
-         #   self.translate(0, -move_amount, 0)
+        if input_object.is_key_pressed(self.KEY_MOVE_DOWN):
+            self.translate(0, -move_amount, 0)
         if input_object.is_key_pressed(self.KEY_TURN_RIGHT) or input_object.mouse_x > 0:
             self.rotate_y(-rotate_amount)
         if input_object.is_key_pressed(self.KEY_TURN_LEFT) or input_object.mouse_x < 0:
